a25586e19d59b7ba69c381e6f63c457d97b0e523
TheWatermelon <bastien.chapusot@gmx.com>
2016-11-23 09:53:29 +0100

    dungeon.start()

--- src/engine/Dungeon.java
+++ src/engine/Dungeon.java
@@ -2,10 +2,18 @@ package engine;
 
 public class Dungeon extends Observer {
 	protected ArrayList<Map> levels;
+	protected int currentLevel;
 	protected Window win;
 	
 	public Dungeon() {
 		levels = new ArrayList<Map>();
+		currentLevel = 0;
+		levels.add(new Map(54, 26));
 		win = new Window();
+	}
+	
+	public void start() {
+		if(win!=null)	win.dispose();
+		win = new Window(levels.get(currentLevel));
 	}
 }
